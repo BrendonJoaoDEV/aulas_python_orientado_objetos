a73f7654ac0246ab32c23c4e9b85233aa7cb6053
BrendonJoaoDEVoficial <brendonjoaodev@gmail.com>
2024-08-21 21:25:26 -0300

    # 002 - aula 001 - Definição de classes - Continuação

---
+++ aula001_definicao_classe/classe_banco_csv.py
@@ -0,0 +1,66 @@
+# Curso de Desenvolvimento de Sistemas.
+# Turma: 0152.
+# Professor: Sebastião Marcos.
+# Data: 21/08/2024.
+
+import os
+import csv
+
+
+class ContaBancaria:
+    def __init__(self, numero_conta, nome_titular, saldo, agencia, tipo_conta):
+        self.numero_conta = numero_conta
+        self.nome_titular = nome_titular
+        self.saldo = saldo
+        self.agencia = agencia
+        self.tipo_conta = tipo_conta
+
+# Função para obter  dados do usuário e criar uma instância de ContaBancaria:
+def obter_dados_conta():
+    numero_conta = input('Digite o número da conta: ')
+    nome_titular = input('Digite o nome do titular: ')
+    saldo = float(input('Digite o saldo inicial: '))
+    agencia = input('Digite a agência: ')
+    tipo_conta = input('Digite o tipo de conta: ')
+    return ContaBancaria(numero_conta, nome_titular, saldo, agencia, tipo_conta)
+
+
+os.system('cls')
+
+# Lista para armazenar contas bancárias:
+contas = []
+
+# Obter dados das contas:
+while True:
+    conta = obter_dados_conta()
+    contas.append({
+        'numero_conta' : conta.numero_conta,
+        'nome_titular' : conta.nome_titular,
+        'saldo' : conta.saldo,
+        'agencia' : conta.agencia,
+        'tipo_conta' : conta.tipo_conta
+    })
+    continuar = input('Deseja adicionar outra conta? (s/n): ')
+    if continuar.lower() != 's':
+        break
+    
+    # Caminho para a pasta onde o arquivo CSV será salvo:
+    pasta = 'aula001_definicao_classe/gravacao_contas/'
+    
+    # VErificando se a pasta existe, se não, irá criá-la:
+    os.makedirs(pasta, exist_ok=True)
+    
+    # Nome para o arquivo CSV para gravar as informações:
+    arquivo = 'aula001_definicao_classe/gravacao_contas/contas.csv'
+    
+    # Escrever a lista de dicionários no arquivo CSV:
+    with open(arquivo, mode='w', newline='') as arquivo_contas:
+        fieldnames = ['numero_conta', 'nome_titular',
+                      'saldo', 'agencia', 'tipo_conta']
+        writer = csv.DictWriter(arquivo_contas, fieldnames=fieldnames, delimiter=';')
+        
+        writer.writeheader()
+        for registro in contas:
+            writer.writerow(registro)
+
+print(f'As informações das contas foram salvas em {arquivo}')
